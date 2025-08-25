b1a2da63ca855d053a56843e8c39652a2a936313
ImVeryBad <80699736+Starexify@users.noreply.github.com>
2025-08-25 20:35:19 +0200

    Now it shows Scale Sword in creative tab

--- src/client/java/net.nova.hexxit_gear_weaponry/HGWClient.java
+++ src/client/java/net.nova.hexxit_gear_weaponry/HGWClient.java
@@ -2,6 +2,11 @@ package net.nova.hexxit_gear_weaponry;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.item.CreativeModeTab;
+import net.nova.hexxit_gear.HexxitGearR;
+import net.nova.hexxit_gear.init.CreativeTab;
 import net.nova.hexxit_gear.init.HGItems;
 import net.nova.hexxit_gear_weaponry.init.HGWItems;
 
@@ -9,8 +14,8 @@ public class HGWClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         // Creative Tab Placement
-  /*      ItemGroupEvents.modifyEntriesEvent().register(entries -> {
-            entries.addAfter(HGItems.SCALE_HELMET, HGWItems.SCALE_SWORD);
-        });*/
+        ItemGroupEvents.modifyEntriesEvent(CreativeTab.HEXXIT_GEAR_TAB_KEY).register(entries -> {
+            entries.addAfter(HGItems.SCALE_BOOTS, HGWItems.SCALE_SWORD);
+        });
     }
 }
