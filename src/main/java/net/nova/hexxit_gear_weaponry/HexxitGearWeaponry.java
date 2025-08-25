13f0627783839bfe41a181928b5706358d8fa490
ImVeryBad <80699736+Starexify@users.noreply.github.com>
2025-08-25 16:37:17 +0200

    1.21.8 Fabric stuff

--- src/main/java/net/nova/hexxit_gear_weaponry/HexxitGearWeaponry.java
+++ src/main/java/net/nova/hexxit_gear_weaponry/HexxitGearWeaponry.java
@@ -1,23 +1,18 @@
 package net.nova.hexxit_gear_weaponry;
 
+import net.fabricmc.api.ModInitializer;
 import net.minecraft.resources.ResourceLocation;
-import net.neoforged.bus.api.IEventBus;
-import net.neoforged.fml.common.Mod;
-import net.nova.hexxit_gear_weaponry.init.HGWDataComponents;
 import net.nova.hexxit_gear_weaponry.init.HGWItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import static net.nova.hexxit_gear_weaponry.HexxitGearWeaponry.MODID;
-
-@Mod(MODID)
-public class HexxitGearWeaponry {
+public class HexxitGearWeaponry implements ModInitializer {
     public static final String MODID = "hexxit_gear_weaponry";
-    public static final Logger logger = LoggerFactory.getLogger(HexxitGearWeaponry.class);
+    public static final Logger LOGGER = LoggerFactory.getLogger(HexxitGearWeaponry.class);
 
-    public HexxitGearWeaponry(IEventBus bus) {
-        HGWDataComponents.COMPONENTS.register(bus);
-        HGWItems.ITEMS.register(bus);
+    @Override
+    public void onInitialize() {
+
     }
 
     // Util methods
